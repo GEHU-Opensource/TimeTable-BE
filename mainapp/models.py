10f63cdb61ee7e53395f0e0dcea97dcc952c6ad4
Vibha <vibhachandola23012005@gmail.com>
2025-06-01 00:14:07 +0530

    added schedule model and a fixup for sending email

--- mainapp/models.py
+++ mainapp/models.py
@@ -308,3 +308,13 @@ class Student(models.Model):
 
     def __str__(self):
         return f"{self.student_name} ({self.student_id}) - Section {self.section}"
+
+
+class Schedule(models.Model):
+    chromosome = models.CharField(max_length=20)
+    day = models.CharField(max_length=20)
+    section = models.CharField(max_length=10)
+    teacher_id = models.CharField(max_length=100)
+    subject_id = models.CharField(max_length=100)
+    classroom_id = models.CharField(max_length=20)
+    time_slot = models.CharField(max_length=20)
